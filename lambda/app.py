4db2176fc47914f418f9e911b7882eedf2e56e3a
EC2 Default User <ec2-user@ip-172-31-43-124.ec2.internal>
2022-06-02 22:48:25 +0000

    added role and function

--- lambda/app.py
+++
@@ -1,8 +0,0 @@
-import json
-# import boto3
-# s3 = boto3.client('s3')
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps("hello from lambda")
-    }
